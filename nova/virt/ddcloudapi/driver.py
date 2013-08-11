f437f51f3bb0d2dd92b7e623f4c9779b4fe65453
johnathonme <Jm968596>
2013-08-11 18:31:31 +0800

    Corrections for libvirt adds to driver.py

--- nova/virt/ddcloudapi/driver.py
+++ nova/virt/ddcloudapi/driver.py
@@ -777,7 +777,6 @@ class CloudcontrolapiDriver(driver.ComputeDriver):
         return
         import sys
         sys.exit()
-        # early exit
         #super(CloudcontrolapiDriver, self).__init__(virtapi)
         self._cluster_name = CONF.ddcloudapi_cluster_name
         if not self._cluster_name:
